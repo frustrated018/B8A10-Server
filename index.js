0122575ab05c2e892ae9a754506d65b7c5db96c5
frustrated018 <niloyd018@gmail.com>
2023-10-18 03:44:42 +0600

    Set up the basic server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) =>{
+    res.send('Clothing Cove server up And running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Running on PORT: ${port}`);
+})
