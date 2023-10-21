4cd44b3e90caa87c52a6bfda3497603f315cae95
frustrated018 <niloyd018@gmail.com>
2023-10-21 12:28:37 +0600

    Added update function to the server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // DB Connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hq7fxqk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,31 @@ async function run() {
       .db("clothingCoveDB")
       .collection("products");
     const brandsCollection = client.db("clothingCoveDB").collection("brands");
+    const cartCollection = client.db("clothingCoveDB").collection("cart");
+
+    // cart route
+
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // adding products to cart
+    app.post("/cart", async (req, res) => {
+      const newProduct = req.body;
+      const result = await cartCollection.insertOne(newProduct);
+      console.log(result);
+      res.send(result);
+    });
+
+    // Deleting for cart
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // brands route
 
@@ -56,21 +81,51 @@ async function run() {
       res.send(result);
     });
 
+    // Updating product in the database
+    app.put("/products/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateProduct = req.body;
+      const product = {
+        $set: {
+          productName: updateProduct.productName,
+          brandName: updateProduct.brandName,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          rating: updateProduct.category,
+          photo: updateProduct.photo,
+          description: updateProduct.description,
+          longerDescription: updateProduct.longerDescription,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        product,
+        options
+      );
+      res.send(result);
+    });
+
     // finding products by brand name
 
     app.get("/products/:brandName", async (req, res) => {
       const brandName = req.params.brandName;
-      const query = { brandName: brandName }; 
+      const query = { brandName: brandName };
       const cursor = productCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // finding product by _id
 
+    app.get("/products/details/:id", async (req, res) => {
+      const id = req.params.id;
 
-
-
-
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
